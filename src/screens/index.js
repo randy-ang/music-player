912c044d0ed94e190d9e7b490b2e404d0dd30605
randy-ang <randyanggadinata@gmail.com>
2021-04-24 19:23:16 +0700

    remove unused things

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,16 +1,10 @@
-import FavoritesScreen from './FavoritesScreen';
 import MusicListScreen from './MusicListScreen';
 
 export const MUSIC_LIST_SCREEN = 'MusicList';
-export const FAVORITES_SCREEN = 'Favorites';
 
 export const SCREENS = [
   {
     name: MUSIC_LIST_SCREEN,
     component: MusicListScreen,
-  },
-  {
-    name: FAVORITES_SCREEN,
-    component: FavoritesScreen,
   },
 ];
