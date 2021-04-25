8342be5fef3f8dac764d89b9cd68b95081cb1407
randy-ang <randyanggadinata@gmail.com>
2021-04-25 20:53:16 +0700

    remove coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -7,12 +7,6 @@ module.exports = {
     '!<rootDir>/jest.config.js',
     '!<rootDir>/dist/**',
   ],
-  coverageThreshold: {
-    global: {
-      statements: 70,
-      branches: 70,
-    },
-  },
   preset: 'jest-expo',
   transformIgnorePatterns: [
     'node_modules/(?!(jest-|@)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)',
